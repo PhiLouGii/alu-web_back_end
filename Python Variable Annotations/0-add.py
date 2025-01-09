827530decc9942eff8623ca242b03386b7852c1a
Philippa Louise Giibwa <125802100+PhiLouGii@users.noreply.github.com>
2025-01-09 19:10:00 +0300

    Delete Python Variable Annotations directory

--- Python Variable Annotations/0-add.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-"""Annotations"""
-
-
-def add(a: float, b:float) -> float:
-    """
-    Function that takes two float arguments 'a' and 'b' and returns their sum as a float.
-    """
-    return a + b 
