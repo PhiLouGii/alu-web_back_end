41f98d6dc2200463a53348d83eb2d5ba0e66207a
Philippa Louise Giibwa <125802100+PhiLouGii@users.noreply.github.com>
2025-01-09 19:22:07 +0300

    Update 8-make_multiplier.py

--- python_variable_annotations/8-make_multiplier.py
+++ python_variable_annotations/8-make_multiplier.py
@@ -9,5 +9,6 @@ def make_multiplier(multiplier: float) -> Callable[[float], float]:
     argument and returns a function that multiplies a float by multiplier.
     """
     def fn(n: float) -> float:
-        return n * multiplier 
+        return n * multiplier
     return fn
+    
