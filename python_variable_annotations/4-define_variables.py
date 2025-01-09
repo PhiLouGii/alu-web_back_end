36864a75681b0be2253ecc14b1b505ec962f9542
Philippa Louise Giibwa <125802100+PhiLouGii@users.noreply.github.com>
2025-01-09 19:34:39 +0300

    Update 4-define_variables.py

--- python_variable_annotations/4-define_variables.py
+++ python_variable_annotations/4-define_variables.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python3
-"""Defining and annotating variables with specified values"""
-
-
+""" Define variables
+    Define and annotate the following variables with the specified values """
+a: int = 1
+pi: float = 3.14
 i_understand_annotations: bool = True
 school: str = 'Holberton'
-pi: float = 3.14
-a: int = 1
