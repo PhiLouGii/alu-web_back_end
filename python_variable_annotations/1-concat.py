d13fee82127cb5e0105e69274893f4a88fae1934
Philippa Louise Giibwa <125802100+PhiLouGii@users.noreply.github.com>
2025-01-09 19:38:05 +0300

    Update 1-concat.py

--- python_variable_annotations/1-concat.py
+++ python_variable_annotations/1-concat.py
@@ -1,11 +1,8 @@
 #!/usr/bin/env python3
-"""Concatenate two strings."""
+""" Basic annotations - concat """
 
 
 def concat(str1: str, str2: str) -> str:
-    """
-    A function that takes two arguments, `str1` and `str2`,
-    which are expected to be strings,
-    and returns the concatenation of these two strings.
-    """
+    """ type-annotated function concat that takes a string str1 and a string
+        str2 as arguments and returns a concatenated string """
     return str1 + str2
